c311da1f625da1beada5408d62bc86318f8558c4
Jo+ <saeedyoussef219@gmail.com>
2025-12-21 01:25:02 +0200

    Fix quotes and update server port

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectToDb from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
-import userRoutes from "./routes/userRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import cartRoutes from "./routes/cartRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import { errorHandler, notFound } from "./middleware/error.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectToDb from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import { errorHandler, notFound } from './middleware/error.js';
 
 dotenv.config();
 const app = express();
@@ -18,23 +18,19 @@ connectCloudinary();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/user", userRoutes);
-app.use("/api/product", productRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/order", orderRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/product', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/order', orderRoutes);
 
-app.use("/images", express.static("uploads"));
-
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
+app.get('/', (req, res) => res.send('API Working'));
 
 app.use(notFound);
 app.use(errorHandler);
 
-if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Server listening on port ${port}!`));
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => console.log(`Server listening on port ${port}!`));
 }
 
 export default app;
