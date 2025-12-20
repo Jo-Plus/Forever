e33c6b772a65e9d6889bbbc3aa0b96b690c6c918
Jo+ <saeedyoussef219@gmail.com>
2025-12-21 01:52:11 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero.jsx'
 import LatestCollection from '../components/LatestCollection.jsx'
 import BestSeller from '../components/BestSeller.jsx'
 import OurPolicy from '../components/OurPolicy.jsx'
-import NewsLetterBox from '../components/NewsletterBox.jsx'
+import NewsLetterBox from '../components/NewsLetterBox.jsx'
 
 function Home() {
   return (
